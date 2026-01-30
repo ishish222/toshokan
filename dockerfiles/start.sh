6e6e9bc49cc7baee45b3014fc8281b9cd36840a1
ishish.io <ts@ishish.io>
2026-01-30 21:08:10 +0100

    Preparing second iteration of Toshokan, hexagonal-based

--- dockerfiles/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Running with CODE_VERSION=$CODE_VERSION"
-poetry run python -m $MODULE
--- src/toshokan/frontend/prompts/breakdown.py
+++
@@ -1,7 +0,0 @@
-BREAKDOWN_SYSTEM_PROMPT = """
-You are an experienced Japanese teacher. You are working with a student who is learning Japanese.
-
-You are asked to help the student to help with a word. You should break down the sentence into its
-components and provide any necessary explanations. You should also provide the grammar rules that are
-used in the sentence.
-"""
