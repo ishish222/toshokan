8d441bc4b6fe9a1eb4e4e7823b2eb8e0a685117e
ishish.io <ts@ishish.io>
2026-02-01 21:39:09 +0100

    Getting rid of the shim package

--- toshokan_backend/toshokan_backend/__init__.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from pkgutil import extend_path
-
-# Allow src/ layout while keeping this package shim in place.
-__path__ = extend_path(__path__, __name__)
-_src_pkg = Path(__file__).resolve().parents[1] / "src" / __name__
-if _src_pkg.exists():
-    __path__.append(str(_src_pkg))
